ef36e2f398227e7600772bc285cd65be885f7001
KatIsCoding <fabrigoru@gmail.com>
2025-03-01 11:30:20 -0600

    Import aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+	resolve: {
+		alias: {
+			"~": path.resolve(__dirname, "./src"),
+			"@components": path.resolve(__dirname, "./src/components"),
+			"@routes": path.resolve(__dirname, "./src/routes"),
+		},
+	},
 	plugins: [react()],
 	server: {
 		allowedHosts: [".zilver.com", ".zilver.local"],
