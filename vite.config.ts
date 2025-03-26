e47e0397a5ec59ea356f920d7c583e28f94cf320
KatIsCoding <fabrigoru@gmail.com>
2025-03-26 09:04:03 -0600

    Revert custom vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,15 @@
-import { defineConfig, Plugin } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-
-function disableCachePlugin(): Plugin {
-	return {
-		name: "strip-if-none-match",
-		handleHotUpdate({ server }) {
-			server.ws.send({ type: "full-reload" });
-			return [];
-		},
-		configureServer(server) {
-			server.middlewares.use((req, _res, next) => {
-				delete req.headers["if-none-match"];
-				next();
-			});
-		},
-	};
-}
 
 // https://vite.dev/config/
 export default defineConfig({
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
-			// "@components": path.resolve(__dirname, "./src/components"),
-			// "@routes": path.resolve(__dirname, "./src/routes"),
-			// "@utils": path.resolve(__dirname, "./src/utils"),
 		},
 	},
-	plugins: [react(), disableCachePlugin()],
+	plugins: [react()],
 	server: {
 		watch: {
 			alwaysStat: true,
