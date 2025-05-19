a234a7ada5ec30fb467308e4ac45f5d9dbb42347
KatIsCoding <fabrigoru@gmail.com>
2025-05-19 16:57:00 -0600

    Import css files

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"; // Query client for the API calls
 const queryClient = new QueryClient();
+import "./index.css";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
