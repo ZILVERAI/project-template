158bfb5e68abd27a4923e714f68370d87b0fc2cc
KatIsCoding <fabrigoru@gmail.com>
2025-06-06 11:29:08 -0600

    Zynapse version bump

--- backend/src/server/implementations/greeting.service.ts
+++ backend/src/server/implementations/greeting.service.ts
@@ -14,7 +14,7 @@ const greetingImplementation = new ServiceImplementationBuilder(
 		async function (input, req, ctx, conn) {
 			for (const letter of input.name.split("")) {
 				await new Promise((resolve) => setTimeout(resolve, 1000));
-				conn.write(letter);
+				await conn.write(letter);
 			}
 			await conn.close();
 		},
