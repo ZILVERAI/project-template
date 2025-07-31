166e67a081cf8d773efe1d6e87e8872e06d3c52d
KatIsCoding <fabrigoru@gmail.com>
2025-07-31 16:43:32 -0600

    Remove initial greeting service file

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,4 +1,3 @@
-import { useGreetingStreamedNameSubscription } from "@/_generated/greeting.service";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
@@ -6,22 +5,9 @@ export const Route = createFileRoute("/")({
 });
 
 function Index() {
-  const { messages } = useGreetingStreamedNameSubscription(
-    {
-      name: "Fabrizio",
-    },
-    {
-      onError: (msg) => {
-        console.error(msg);
-      },
-    },
-  );
   return (
     <div className="p-2">
       <h3>Welcome Home!</h3>
-      {messages.map((m) => {
-        return <h1>{m}</h1>;
-      })}
     </div>
   );
 }
