bdf368f0869e26ee9bfb1df6af88e596e76c5f89
KatIsCoding <fabrigoru@gmail.com>
2025-08-02 12:04:21 -0600

    Initial migration files

---
+++ backend/migrations/0000_initial_db.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `users_table` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`name` text NOT NULL,
+	`age` integer NOT NULL,
+	`email` text NOT NULL,
+	`something` text NOT NULL,
+	`somethingElse` text NOT NULL
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `users_table_email_unique` ON `users_table` (`email`);
