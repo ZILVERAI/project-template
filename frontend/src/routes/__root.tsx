1a598223a00e7ad66e9a97e1aea23bf824244060
Fabrizio Gomez Ruiz <fabrigoru@gmail.com>
2025-10-25 01:09:09 -0600

    Use tanstack form and remove the other one

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,19 +1,6 @@
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
-
-const sampleSchema = z.object({
-  hello: z.string(),
-});
 
 function RootComponent() {
-  useForm({
-    resolver: zodResolver(sampleSchema),
-    defaultValues: {
-      hello: "",
-    },
-  });
   return (
     <>
       <div className="p-2 flex gap-2">
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,13 +1,58 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { useForm } from "@tanstack/react-form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { z } from "zod";
+import { useState } from "react";
 
 export const Route = createFileRoute("/")({
   component: Index,
 });
 
+const sampleSchema = z.object({
+  message: z.string(),
+});
+
 function Index() {
+  const [msg, setMsg] = useState<string | undefined>(undefined);
+  const form = useForm({
+    defaultValues: {
+      message: "",
+    },
+    validators: {
+      onChange: sampleSchema,
+    },
+    onSubmit: ({ value }) => {
+      setMsg(value.message);
+    },
+  });
+
   return (
     <div className="p-2">
       <h3>Welcome Home!</h3>
+      {msg && <h3>{msg}</h3>}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          form.handleSubmit();
+        }}
+      >
+        <form.Field
+          name="message"
+          children={(field) => {
+            return (
+              <>
+                <Input
+                  value={field.state.value}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                />
+              </>
+            );
+          }}
+        />
+        <Button type="submit">Go</Button>
+      </form>
     </div>
   );
 }
